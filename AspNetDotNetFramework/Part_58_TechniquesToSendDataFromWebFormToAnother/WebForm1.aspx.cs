f29339a361564f4f57a304e8c3d636c6346e04f8
avek1 <jlfloresjr@gmail.com>
2020-03-22 21:08:26 -0700

    part 58 for realz

---
+++ AspNetDotNetFramework/Part_58_TechniquesToSendDataFromWebFormToAnother/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Part_58_TechniquesToSendDataFromWebFormToAnother
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // technique 1
+            Server.Transfer("WebForm2.aspx");
+        }
+
+
+        // technique 2
+        public string Name
+        {
+            get
+            {
+                return txtName.Text;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return txtEmail.Text;
+            }
+        }
+    }
+}
---
+++ AspNetDotNetFramework/Part_58_TechniquesToSendDataFromWebFormToAnother/WebForm2.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Part_58_TechniquesToSendDataFromWebFormToAnother
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // technique 1
+            //if (!IsPostBack)
+            //{
+            //    Page lastPage = (Page)Context.Handler;
+            //    lblName.Text = ((TextBox)lastPage.FindControl("txtName")).Text;
+            //    lblEmail.Text = ((TextBox)lastPage.FindControl("txtEmail")).Text;
+            //}
+
+            // technique 2
+            if (!IsPostBack)
+            {
+                Page lastPage = (Page)Context.Handler;
+                if (lastPage is WebForm1)
+                {
+                    lblName.Text = ((WebForm1)lastPage).Name;
+                    lblEmail.Text = ((WebForm1)lastPage).Email;
+
+                }
+            }
+        }
+    }
+}
