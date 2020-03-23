0e4591c46e24eadf23bad34a034f7a1abacaba58
avek1 <jlfloresjr@gmail.com>
2020-03-22 22:29:05 -0700

    part 59 query string

---
+++ AspNetDotNetFramework/Part_59_QueryString/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Part_59_QueryString")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part_59_QueryString")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f5a2c676-efc9-44ca-9477-00ec1c682f75")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AspNetDotNetFramework/Part_59_QueryString/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Part_59_QueryString
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSendData_Click(object sender, EventArgs e)
+        {
+            // technique 1
+            //Response.Redirect("~/WebForm2.aspx?UserName=" + txtName.Text + "&Email=" + txtEmail.Text);
+
+            // technique 2 - i like this one.
+            Response.Redirect("~/WebForm2.aspx?UserName=" + Server.UrlEncode(txtName.Text) + "&Email=" + Server.UrlEncode(txtEmail.Text));
+
+            // technique 3
+            //Response.Redirect("~/WebForm2.aspx?UserName=" + txtName.Text.Replace("&", "%26") + "&Email=" + txtEmail.Text.Replace("&", "%26"));
+
+        }
+    }
+}
