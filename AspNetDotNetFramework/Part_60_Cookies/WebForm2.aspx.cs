bc65c44a60710e5e0b2b259e041f6efd02f8bb21
avek1 <jlfloresjr@gmail.com>
2020-03-28 11:00:15 -0700

    part 60 - cookies.  fyi google holds cookies even without experiation date.  ie works fine.

---
+++ AspNetDotNetFramework/Part_60_Cookies/WebForm2.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+
+namespace Part_60_Cookies
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Request.Cookies["UserInfo"];
+            if (cookie != null)
+            {
+                lblName.Text = cookie["Name"];
+                lblEmail.Text = cookie["Email"];
+            }
+        }
+    }
+}
